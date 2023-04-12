9791010b080671aed9592ab382aa88b0722c97f1
Galyaminsky <jobforandroid@proton.me>
2023-04-12 10:15:47 +0600

    HomeWork_#2 Completed #1

--- app/src/main/java/me/proton/jobforandroid/gb_popularlib_moxydaggerrxjava/domain/UsersRepo.kt
+++
@@ -1,8 +0,0 @@
-package me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava.domain
-
-interface UsersRepo {
-    fun getUsers(
-        onSuccess: (List<UserEntity>) -> Unit,
-        onError: ((Throwable) -> Unit)? = null
-    )
-}
