99f1e6b917852c8d98ef2848760e5cc05805590e
Galyaminsky <jobforandroid@proton.me>
2023-04-12 10:34:14 +0600

    HomeWork_#3 Completed #1

--- app/src/main/java/me/proton/jobforandroid/gb_popularlib_moxydaggerrxjava/App.kt
+++ app/src/main/java/me/proton/jobforandroid/gb_popularlib_moxydaggerrxjava/App.kt
@@ -3,27 +3,20 @@ package me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava
 import android.app.Application
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
-import me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava.data.OurUsersRepoImpl
-import me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava.domain.GithubUsersRepo
-import me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava.domain.cicerone.CiceronePresenter
 
 class App : Application() {
+    companion object {
+        lateinit var instance: App
+    }
+
     private val cicerone: Cicerone<Router> by lazy {
         Cicerone.create()
     }
-    val router get() = cicerone.router
     val navigatorHolder get() = cicerone.getNavigatorHolder()
-    val navigation get() = CiceronePresenter()
-
-    val usersRepo: GithubUsersRepo by lazy { OurUsersRepoImpl() }
+    val router get() = cicerone.router
 
     override fun onCreate() {
         super.onCreate()
-        INSTANCE = this
-    }
-
-    companion object {
-        internal lateinit var INSTANCE: App
-            private set
+        instance = this
     }
 }
