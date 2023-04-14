8d7afd56aa0f5f5d37dc8dcb5997b644eba1dc98
Galyaminsky <jobforandroid@proton.me>
2023-04-14 23:33:36 +0600

    HomeWork_#7 COMPLETED

--- app/src/main/java/me/proton/jobforandroid/gb_popularlib_moxydaggerrxjava/App.kt
+++ app/src/main/java/me/proton/jobforandroid/gb_popularlib_moxydaggerrxjava/App.kt
@@ -4,19 +4,21 @@ package me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava
 import com.github.terrakok.cicerone.Cicerone
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-
-
+import io.reactivex.rxjava3.*
 import kotlinx.coroutines.withContext
+import me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava.helpers.di.DaggerAppComponent
 import me.proton.jobforandroid.gb_popularlib_moxydaggerrxjava.helpers.schedules.AppSchedulersImpl
+
 
 
 class App : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<App> =
-        DaggerApplication
+        DaggerAppComponent
             .builder()
             .apply {
                 withContext(applicationContext)
+
                 val cicerone = Cicerone.create()
                 withNavigationHolder(cicerone.getNavigatorHolder())
                 withRouter(cicerone.router)
